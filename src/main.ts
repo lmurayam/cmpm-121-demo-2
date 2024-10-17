2643ffb542d58a60935f34ea1fb5c46ed3d666ea
lmurayam <130516683+lmurayam@users.noreply.github.com>
2024-10-17 19:53:33 +0000

    Added Canvas and Title - Step 1

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,17 @@ const APP_NAME = "Test";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+//app.innerHTML = APP_NAME;
+
+const app_title : HTMLHeadingElement = document.createElement("h1");
+app_title.innerHTML = APP_NAME;
+app_title.style.textAlign = "center"
+app_title.style.position = "absolute";
+app_title.style.top = "5%";
+app_title.style.left = "50%";
+app_title.style.transform = "translateX(-50%)";
+app.append(app_title);
+
+const canvas : HTMLCanvasElement = document.createElement("canvas");
+canvas.width = canvas.height = 256;
+app.append(canvas);
