000dac5d206f865770f293d8c9e380ac6c4a7144
lmurayam <130516683+lmurayam@users.noreply.github.com>
2024-10-20 23:17:26 +0000

    Simple Drawing - Step 2

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ const APP_NAME = "Test";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-//app.innerHTML = APP_NAME;
 
 const app_title : HTMLHeadingElement = document.createElement("h1");
 app_title.innerHTML = APP_NAME;
@@ -18,3 +17,39 @@ app.append(app_title);
 const canvas : HTMLCanvasElement = document.createElement("canvas");
 canvas.width = canvas.height = 256;
 app.append(canvas);
+
+const ctx : CanvasRenderingContext2D | null = canvas.getContext("2d");
+
+const ui_elements : HTMLHeadingElement = document.createElement("h4");
+app.append(ui_elements);
+
+const clear_button : HTMLButtonElement = document.createElement("button");
+clear_button.innerHTML = "clear"
+ui_elements.append(clear_button);
+clear_button.addEventListener("click",()=>{
+    ctx?.clearRect(0,0,canvas.width,canvas.height);
+});
+
+//Source: https://quant-paint.glitch.me/paint0.html
+
+
+const cursor = {active: false, x:0, y:0};
+
+canvas.addEventListener("mousedown", (e)=>{
+    cursor.active = true;
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+});
+canvas.addEventListener("mouseup",()=>{
+    cursor.active=false;
+});
+canvas.addEventListener("mousemove",(e)=>{
+    if(cursor.active&&ctx!=null){
+        ctx.beginPath();
+        ctx.moveTo(cursor.x,cursor.y);
+        ctx.lineTo(e.offsetX,e.offsetY);
+        ctx.stroke();
+        cursor.x = e.offsetX;
+        cursor.y = e.offsetY;
+    }
+});
