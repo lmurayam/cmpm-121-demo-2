a87f76714dd12c455696700dfd410eca61c5b4c3
lmurayam <130516683+lmurayam@users.noreply.github.com>
2024-10-21 00:49:58 +0000

    Undo/Redo - Step 4

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import "./style.css";
 
-const APP_NAME = "Test";
+const APP_NAME = "DRAW";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
@@ -29,10 +29,31 @@ ui_elements.append(clear_button);
 clear_button.addEventListener("click",()=>{
     clearCanvas();
     //I think in a lecture the prof said not to do this...
-    points.length=0;
+    lines.length=0;
+    redo_lines.length=0;
+});
+
+const undo_button : HTMLButtonElement = document.createElement("button");
+undo_button.innerHTML = "undo"
+ui_elements.append(undo_button);
+undo_button.addEventListener("click",()=>{
+    const last_point = lines.pop();
+    if(last_point!=null){redo_lines.push(last_point)};
+    canvas.dispatchEvent(drawing_changed);
+});
+
+const redo_button : HTMLButtonElement = document.createElement("button");
+redo_button.innerHTML = "redo"
+ui_elements.append(redo_button);
+redo_button.addEventListener("click",()=>{
+    const last_point = redo_lines.pop();
+    if(last_point!=null){lines.push(last_point)};
+    canvas.dispatchEvent(drawing_changed);
 });
 
 type point = [number,number];
+
+type line = point[];
 
 interface ICursor {
     active: boolean;
@@ -41,22 +62,30 @@ interface ICursor {
 
 const cursor : ICursor = {active:false, point: [0,0]};
 
-const points : point[] = [];
+const lines : line[] = [];
+const redo_lines: line[] = [];
+
+let current_line : line = [];
 
 const drawing_changed : Event = new CustomEvent("drawing-changed");
 
 canvas.addEventListener("mousedown", (e)=>{
     cursor.active = true;
     cursor.point = [e.offsetX,e.offsetY];
+    current_line = [cursor.point];
+    lines.push(current_line);
+    canvas.dispatchEvent(drawing_changed);
+
+    redo_lines.length = 0;
 });
 addEventListener("mouseup",()=>{
     cursor.active=false;
+    canvas.dispatchEvent(drawing_changed);
 });
 canvas.addEventListener("mousemove",(e)=>{
     if(cursor.active){
-        points.push(cursor.point);
-        points.push([e.offsetX,e.offsetY]);
-        cursor.point = [e.offsetX,e.offsetY]
+        cursor.point = [e.offsetX,e.offsetY];
+        current_line.push(cursor.point)
         canvas.dispatchEvent(drawing_changed);
     }
 });
@@ -70,11 +99,13 @@ function clearCanvas() {
 }
 
 function redrawCanvas(){
-    for(let i = 0; i<points.length;i+=2){
+    lines.forEach((line)=>{
         ctx?.beginPath();
-        ctx?.moveTo(...points[i]);
-        ctx?.lineTo(...points[i+1]);
+        ctx?.moveTo(...line[0]);
+        line.forEach((point)=>{
+            ctx?.lineTo(...point);
+        })
         ctx?.stroke();
-    }
+    })
 }
 
